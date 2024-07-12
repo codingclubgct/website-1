a465b94a2153412571ca54f5ee53aea9406af917
useEffects <useEffects@users.noreply.github.com>
2024-07-12 17:46:17 +0000

    deploy: coding-club-gct/front-gate@86f0cce65c5b7f2278098dbaac7c036b4e500055

--- apps/web-ring/src/app/layout.tsx
+++ apps/web-ring/src/app/layout.tsx
@@ -3,23 +3,20 @@ import { ThemeProvider } from "@/context/mui";
 import "./globals.css"
 import { DarkModeProvider } from "@/context/darkmode";
 import { CatppuccinProvider } from "@/context/catppuccin";
+import { ReactNode } from "react";
 
-import { Metadata } from "next";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" id="__next">
       <body>
-      
-          <DarkModeProvider>
-            <ThemeProvider>
-              <CatppuccinProvider>
-               
-                    {children}
-          
-              </CatppuccinProvider>
-            </ThemeProvider>
-          </DarkModeProvider>
+
+        <DarkModeProvider>
+          <ThemeProvider>
+            <CatppuccinProvider>
+              {children}
+            </CatppuccinProvider>
+          </ThemeProvider>
+        </DarkModeProvider>
       </body>
     </html>
   );
